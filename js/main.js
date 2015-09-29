328b1ccc107b3017d46ad8ba0b59d050c2c57f91
itf-lajos <itf.lajos@gmail.com>
2015-09-29 17:08:07 +0200

    Második videó vége

--- js/main.js
+++ js/main.js
@@ -6,29 +6,55 @@ var audioHandler = function ( div ) {
     this.div = div;
     this.playBtn = div.querySelector( ".play" );
     this.muteBtn = div.querySelector( ".mute" );
+    this.fullScreenBtn = div.querySelector( ".full-screen" );
     this.audio = div.querySelector( "audio" );
+    this.video = div.querySelector( "video" );
     
     // Constructor.
     this.construct = function () {
+        // Ha nincs audio elemünk, akkor videot kezelünk és fordítva
+        this.mediaElement = this.audio == null ? this.video : this.audio;
+        if ( this.mediaElement == null ) {
+            console.error( "Nem található média elem!" );
+            return;
+        }
+        
         // Lejátszó gomb
+        var self = this;
         this.playBtn.addEventListener( "click", function() {
 //            console.log( this );
-            this.parentNode.handler.togglePlay();
+            self.div.handler.togglePlay();
+//            this.parentNode.handler.togglePlay();
         }, false );
         this.muteBtn.addEventListener( "click", function() {
 //            console.log( this );
-            this.parentNode.handler.toggleMute();
+            self.div.handler.toggleMute();
+//            this.parentNode.handler.toggleMute();
         }, false );
+        
+        // Ha a full-screen gomb létezik, beállítjuk
+        if ( this.fullScreenBtn !== null ) {
+            this.fullScreenBtn.addEventListener( "click", function() {
+                if ( self.video.parentNode.classList.contains( "col-md-12" )) {
+                    self.video.parentNode.classList.remove( "col-md-12" );
+                    self.video.parentNode.classList.add( "col-md-3" );
+                } else {
+                    self.video.parentNode.classList.remove( "col-md-3" );
+                    self.video.parentNode.classList.add( "col-md-12" );
+                }   
+            }, false );    
+        }
+        
     };
     
     // Lejátszás.
     this.play = function() {
-        this.audio.play();
+        this.mediaElement.play();
     };
     
     // Lejátszás váltása.
     this.togglePlay = function() {
-        if ( this.audio.paused ) {
+        if ( this.mediaElement.paused ) {
             this.play();
             this.div.classList.add( "played" );
         } else {
@@ -39,21 +65,21 @@ var audioHandler = function ( div ) {
     
     // Megállítás.
     this.pause = function() {
-        this.audio.pause();
+        this.mediaElement.pause();
     };
 
     // Némítás.
     this.muted = function() {
-        this.audio.muted = true;
+        this.mediaElement.muted = true;
     };
 
     // Némítás váltása.
     this.toggleMute = function() {
-        if ( this.audio.muted ) {
-            this.audio.muted = false;
+        if ( this.mediaElement.muted ) {
+            this.mediaElement.muted = false;
             this.div.classList.remove( "muted" );
         } else {
-            this.audio.muted = true;
+            this.mediaElement.muted = true;
             this.div.classList.add( "muted" );
         }    
 //        this.toggleMute = function() {
@@ -86,7 +112,7 @@ var audioHandler = function ( div ) {
     this.construct();
 };
 
-var audioDivs = document.querySelectorAll( ".audio-handler-div");
+var audioDivs = document.querySelectorAll( ".media-handler-div");
 Array.prototype.forEach.call( audioDivs, function( item ) {
     item.handler = new audioHandler( item );
 } );    
